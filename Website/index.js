c6b5be979a4ff0648d977b74344aad4f3eb9b11c
linusnyren <linusny@hotmail.com>
2019-04-29 12:14:27 +0200

    Search by author on website

--- Website/index.js
+++ Website/index.js
@@ -3,11 +3,12 @@ new Vue({
   data: {
     author: null,
     message: null,
+    findAuthor: null,
     notes: null
   },
   el: '#app',
   created() {
-      fetch('http://localhost:8080/GuestBook/GuestBook/GuestBook')
+      fetch('http://localhost:8080/GuestBook/GuestBook/GuestBook/')
           .then(response => response.json())
           .then(result => {
               let list = []
@@ -16,24 +17,31 @@ new Vue({
                 "message": result[i].message,
                 "id": result[i].id,
                 "author": result[i].author,
-                "date": (new Date(parseInt(result[i].date))).toString().substring(0,21)
+                "date": (new Date(parseInt(result[i].date))).toString().substring(3,10)+(new Date(parseInt(result[i].date))).toString().substring(15,21)
               })
             }
             this.notes = list;
           })
   },
   methods: {
-    addNote() {
-      let bodystring = '{ "author": "' +this.author +'", "message": "' +this.message +'"}'
-      fetch('http://localhost:8080/GuestBook/GuestBook/GuestBook', {
-
-        body: bodystring,
-        headers: {
-          "Content-Type": 'application/json'
-        },
-        method: 'POST',
-
-      }).then(fetch('http://localhost:8080/GuestBook/GuestBook/GuestBook')
+    findbyauthor(){
+          fetch('http://localhost:8080/GuestBook/GuestBook/GuestBook/author/'+this.findAuthor)
+              .then(response => response.json())
+              .then(result => {
+                  let list = []
+                  for (var i = 0; i < result.length; i++) {
+                  list.unshift({
+                    "message": result[i].message,
+                    "id": result[i].id,
+                    "author": result[i].author,
+                    "date": (new Date(parseInt(result[i].date))).toString().substring(3,10)+(new Date(parseInt(result[i].date))).toString().substring(15,21)
+                  })
+                }
+                this.notes = list;
+              })
+      },
+      getAll(){
+            fetch('http://localhost:8080/GuestBook/GuestBook/GuestBook/')
                 .then(response => response.json())
                 .then(result => {
                     let list = []
@@ -42,7 +50,32 @@ new Vue({
                       "message": result[i].message,
                       "id": result[i].id,
                       "author": result[i].author,
-                      "date": (new Date(parseInt(result[i].date))).toString().substring(0,21)
+                      "date": (new Date(parseInt(result[i].date))).toString().substring(3,10)+(new Date(parseInt(result[i].date))).toString().substring(15,21)
+                    })
+                  }
+                  this.notes = list;
+                })
+        },
+    addNote() {
+      let bodystring = '{ "author": "' +this.author +'", "message": "' +this.message +'"}'
+      fetch('http://localhost:8080/GuestBook/GuestBook/GuestBook/', {
+
+        body: bodystring,
+        headers: {
+          "Content-Type": 'application/json'
+        },
+        method: 'POST',
+
+      }).then(fetch('http://localhost:8080/GuestBook/GuestBook/GuestBook/')
+                .then(response => response.json())
+                .then(result => {
+                    let list = []
+                    for (var i = 0; i < result.length; i++) {
+                    list.unshift({
+                      "message": result[i].message,
+                      "id": result[i].id,
+                      "author": result[i].author,
+                      "date": (new Date(parseInt(result[i].date))).toString().substring(3,10)+(new Date(parseInt(result[i].date))).toString().substring(15,21)
                     })
                   }
                   this.notes = list;
